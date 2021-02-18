cfeffc9c8a509032b0b39fbee0219ce696457465
Minsik113 <hyunicho62@gmail.com>
2021-02-18 20:51:57 +0900

    2/18 dp 앞부분

--- 문제풀이/test.py
+++ 문제풀이/test.py
@@ -1,31 +1,8 @@
-# # 파일열어 내용보기
-# f = open('C:\\Users\\pc\\Desktop\\buy_list.txt', 'rt') # r = 읽기모드, t = 텍스트파일
-# lines = f.readlines()
-# print(lines)
+n = int(input())
+d = [0] * (1001)
+d[1] = 1
+d[2] = 3
 
-# for line in lines:
-#     print(line, end='')
-# print()
-# for line in lines:
-#     nline = line.split('\n')[0]
-#     print(nline)
-# # print()
-
-# # 파일에 글쓰기
-# f = open('C:\\Users\\pc\\Desktop\\sell_list.txt', 'wt')
-# f.write('삼성전자\n')
-# f.write('하이닉스\n')
-# f.close()
-
-# 1~10까지 txt파일에 쓰기.
-# f = open('C:\\Users\\pc\\Desktop\\sell_list.txt', 'wt') # 기존에 있던말던 첨부터 다시쓰기
-# f = open('C:\\Users\\pc\\Desktop\\sell_list.txt', 'a') # 이어서쓰기
-# for i in range(1,11):
-#     a = str(i)+'\n'
-#     f.write(a)
-
-# 위에 코드를 python스럽게
-# with open('C:\\Users\\pc\\Desktop\\sell_list.txt', 'a') as f:
-#     for i in range(1,11):
-#         a = str(i)+'\n'
-#         f.write(a)
+for i in range(3, n+1):
+    d[i] = (d[i-1] + 2*d[i-2]) % 796796
+print(d[n])
---
+++ 복습/DP_다이나믹프로그래밍/n00_효율적인화폐구성.py
@@ -0,0 +1,54 @@
+'''
+d[i] = 금액i를 만들 수 있는 최소한의 화폐 개수
+
+
+'''
+########################################
+########################################
+#
+n, m = map(int, input().split())
+array = []
+for _ in range(n):
+    array.append(int(input()))
+
+dp = [10001] * (m+1) # m개로 다 체크해볼것이다.
+dp[0] = 0 
+
+for i in range(n):
+    for j in range(array[i], m+1):
+        if dp[j-array[i]] != 10001:
+            dp[j] = min(dp[j], dp[i-array[i]]+1)
+if dp[m] == 10001:
+    print(-1)
+else:
+    print(dp[m])
+
+########################################
+########################################
+'''1. 동전내림차순
+2. 앞부터 하나씩 뺄수있으면 뺸다.
+3. -해서 0이되면 끝 
+4. 모든수 뻇는데 0이아니라 음수로가면 -1출력
+-> 내풀이 - 그리디 - 이렇게 풀면안된다네?
+
+-> 정렬 O(nlogn) = 200 (n=10^2니까)
+-> m번을 다 봐야함 O(M) = 10^4
+-> O(M x NlogN) = 2x10^6 충분하지않나.?
+'''
+# n, m = map(int, input().split())
+# array = []
+
+# for _ in range(n):
+#     array.append(int(input()))
+# array.sort(reverse=True)
+
+# count = 0
+# for i in range(n):
+#     temp = m // array[i]
+#     if temp != 0: # array[i]보다크면 그만큼 뺴줌
+#         m -= temp*array[i]
+#         count += temp
+# if m == 0:
+#     print(count)
+# else:
+#     print(-1)
