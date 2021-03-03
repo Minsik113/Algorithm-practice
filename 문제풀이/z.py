60edd85eaf3b3d47c84dedb19e1713971dea48a8
Minsik113 <hyunicho62@gmail.com>
2021-03-03 22:53:50 +0900

    3/3프로그래머스 2문제 풀기

--- 문제풀이/z.py
+++ 문제풀이/z.py
@@ -1,22 +1,7 @@
-from itertools import permutations
-numbers = "011"
-answer = 0
-data = list(map(str, numbers))
-length = len(numbers)
+a = [1,2,3]
+b = [1,2,3,4]
+if a == b[:len(a)]:
+    print('w')
 
-save = dict()
-for i in numbers:
-    if i not in save:
-        save[i] = 0
-for i in range(2, length+1):
-    for j in permutations(data,i):
-        print(j)
-        # print(list(j))
-        if j not in save:
-            save[j] = 0
-print(save)
-
-for num in save.keys():
-    print("".join(num))
-for num in save.keys():
-    print(type("".join(num)))
+print(a)
+print(b[:len(a)])
