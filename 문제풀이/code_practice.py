9ae924688047149674502d265e5d942e2ec47b43
Minsik113 <hyunicho62@gmail.com>
2021-03-03 18:15:43 +0900

    DFS/BFS 복습

--- 문제풀이/code_practice.py
+++ 문제풀이/code_practice.py
@@ -1,7 +1,59 @@
-array=[
-    [1,2,3]
-    ,[1,2,3]
-    ,[1,2,3]
-]
-a = [count for _ in range(3) for j in range(3) if array[i][j] == 3 count += 1]
-print(a)
+'''
+start부터 출발해서 최단거리가 k인애들 다 출력해라
+없으면 -1
+
+1. BFS로 start부터 각 도시까지 거리 구한다.
+2. 거리k인애들 저장한다. (flag = True)
+3. 거리가 k+1이 되었을때 반복문 나온다.
+4. flag = False라면 그런 도시 없으니 -1 출력
+
+'''
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, m, k, start = map(int, input().split())
+array = [[] for i in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    array[a].append(b)
+# 예외처리
+# if k == 0:
+#     print(-1)
+#     exit()
+# 탐색시작 
+result = []
+flag = False
+def bfs(array, visited, start, result):
+    global flag
+    q = deque()
+    t = 0
+    q.append((start,t))
+    visited[start] = 0
+    while q:
+        v, time = q.popleft()
+        # 찾고자 하는것
+        if time == k:
+            result.append((v))
+            flag = True
+        # 더 안봐도 됨
+        if time > k:
+            break
+        for i in array[v]:
+            if visited[i] == -1: # 안가본애라면 간다
+                q.append((i,time+1))
+                visited[i] = time+1
+                # 찾는애라면 result에 넣음
+                
+
+visited = [-1] * (n+1) # 갈 수 있는 거리를 저장하자
+bfs(array, visited, start, result)
+# print(visited)
+# 답이 존재하지않으면
+result.sort()
+if not flag:
+    print(-1)
+else:
+    for i in result:
+        print(i)
+
--- 복습/3_BFS_DFS/n1_특정거리의도시찾기.py
+++ 복습/3_BFS_DFS/n1_특정거리의도시찾기.py
@@ -13,42 +13,101 @@ m개의 도로
 
 -> level == k 라면 그때 출력 level > k면 break
 '''
+'''
+start부터 출발해서 최단거리가 k인애들 다 출력해라
+없으면 -1
+
+1. BFS로 start부터 각 도시까지 거리 구한다.
+2. 거리k인애들 저장한다. (flag = True)
+3. 거리가 k+1이 되었을때 반복문 나온다.
+4. flag = False라면 그런 도시 없으니 -1 출력
+
+'''
+##########################################
+##########################################
+# 3/3 복습
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, m, k, start = map(int, input().split())
+array = [[] for i in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    array[a].append(b)
+# 예외처리
+# if k == 0:
+#     print(-1)
+#     exit()
+# 탐색시작 
+result = []
+flag = False
+def bfs(array, visited, start, result):
+    global flag
+    q = deque()
+    t = 0
+    q.append((start,t))
+    visited[start] = 0
+    while q:
+        v, time = q.popleft()
+        # 찾고자 하는것
+        if time == k:
+            result.append((v))
+            flag = True
+        # 더 안봐도 됨
+        if time > k:
+            break
+        for i in array[v]:
+            if visited[i] == -1: # 안가본애라면 간다
+                q.append((i,time+1))
+                visited[i] = time+1
+                # 찾는애라면 result에 넣음
+
+visited = [-1] * (n+1) # 갈 수 있는 거리를 저장하자
+bfs(array, visited, start, result)
+# 답이 존재하지않으면
+result.sort()
+if not flag:
+    print(-1)
+else:
+    for i in result:
+        print(i)
 ##########################################
 ##########################################
 # 다시품
-from collections import deque
+# from collections import deque
 
-# 도시개수, 도로개수, 찾는거리값, 시작도시
-n, m, k, start = map(int, input().split())
-graph = [[] for i in range(n+1)] # 1~n
-# 맵 입력
-for i in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
+# # 도시개수, 도로개수, 찾는거리값, 시작도시
+# n, m, k, start = map(int, input().split())
+# graph = [[] for i in range(n+1)] # 1~n
+# # 맵 입력
+# for i in range(m):
+#     a, b = map(int, input().split())
+#     graph[a].append(b)
 
-# 거리찾기
-visited = [False] * (n+1)
-result = []
-def bfs(graph, start, visited):
-    global result
-    q = deque()
-    q.append((0,start))
-    visited[start] = True
-    while q:
-        level, v = q.popleft()
-        if level == k:
-            result.append(v)
-        for i in graph[v]:
-            if not visited[i]:
-                visited[i] = True
-                q.append((level+1,i))
+# # 거리찾기
+# visited = [False] * (n+1)
+# result = []
+# def bfs(graph, start, visited):
+#     global result
+#     q = deque()
+#     q.append((0,start))
+#     visited[start] = True
+#     while q:
+#         level, v = q.popleft()
+#         if level == k:
+#             result.append(v)
+#         for i in graph[v]:
+#             if not visited[i]:
+#                 visited[i] = True
+#                 q.append((level+1,i))
 
-bfs(graph, start, visited)
-result.sort()
-if len(result) == 0:
-    print(-1)
-for i in result:
-    print(i)
+# bfs(graph, start, visited)
+# result.sort()
+# if len(result) == 0:
+#     print(-1)
+# for i in result:
+#     print(i)
 ##########################################
 ##########################################
 # 내풀이 - queue에 (도시,level) 넣음
