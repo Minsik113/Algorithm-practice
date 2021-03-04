b3290ae56b194fbc361305761abd60c43f508b95
Minsik113 <hyunicho62@gmail.com>
2021-03-04 22:50:07 +0900

    이진탐색 복습

---
+++ 복습/5_이진탐색/4_가사검색.py
@@ -0,0 +1,67 @@
+# 효율성 2개틀림.
+# '''
+# 찾는 단어수(10만) x 찾을곳(10만) = 너무많음. 
+# 10만 x O(log(len(words)))으로 줄여야함.
+# (array리스트 => words에 queries[i] 가 들어있는 단어들의 총 개수를 저장)
+
+# 1번. words를 일단 정렬한다. 200만안됨.
+# 단어길이 오름차순, 알파벳순 오름차순
+# 2번. queries에서 단어 꺼내서 비교하자
+# 3번. "찾는 단어의 길이"에 해당하는 '시작index' 와 '끝index'을 찾는다.
+# 4번. 3번이 존재한다면 대입해서 하나씩본다
+# 4-1번. 하나라도 다르면 다른것임.
+# 5번. 3번이 존재안하면 0 리턴
+# '''
+# from bisect import bisect_left, bisect_right
+
+# # target과 같은 길이의 시작index와 끝index를 찾는다. (이때, 끝index는 -1 해야함)(다음위치기 때문에)
+# def find_index(array, length):
+#     left_index = bisect_left(array, length)
+#     right_index = bisect_right(array, length)
+#     return (left_index, right_index)
+    
+# def solution(words, queries):
+#     answer = []
+#     # 1번. (길이, 사전)순서
+#     words = sorted(words, key=lambda x:(len(x),x))
+#     tree = dict() # 한번 본애들은 여기에 저장해두자.
+#     sub_tree = dict() 
+#     # 단어의 길이만 저장된 리스트 생성
+#     array = []
+#     for i in words:
+#         array.append(len(i))
+#     # print(words)
+#     #2번. 단어 찾아보자 (queries에서 words안에 있는지 찾는것이다.)
+#     for name in queries:
+#         count = 0 # 단어가 몇개나 같은지 체크
+#         # 3번. "찾는 단어의 길이"에 해당하는 '시작index' 와 '끝index'을 찾는다.
+#         if name in sub_tree: # ------효율성 증가시키려고 사전에 봤던거 저장해둠
+#             l_index, r_index = sub_tree[name]
+#         else:
+#             l_index, r_index = find_index(array, len(name))
+#             sub_tree[name] = (l_index,r_index)
+#         # 5번. 찾고자 하는 단어의 길이가 없다.
+#         if r_index - l_index == 0: 
+#             answer.append(0)
+#         # 4번. 찾고자 하는 단어의 길이가 있다.
+#         else:
+#             # 4-1번. '해당 범위 안에 같은 문자가 있나' 체크한다
+#             if name in tree: # ------효율성 증가하기위해 사전에 봤던거 저장해둠
+#                 answer.append(tree[name])
+#             else:
+#             # name이 words안에 있는지 체크. (words = 길이오름차순, 사전순)
+#                 for i in range(l_index, r_index, 1): # l_index ~ r_index-1 까지의 words안에 수를 본다
+#                     flag = True # 다른문자있다면 False
+#                     for j in range(len(name)):
+#                         if name[j] == '?': # 무조건 같음
+#                             continue
+#                         if name[j] != words[i][j]: # 다르다면 다음 words랑 비교해본다
+#                             flag = False
+#                             break
+#                     # words[i]와 name이 같다면) 개수 센다. 다음 문자 확인하러가자.
+#                     if flag == True: 
+#                         count+=1
+#                 answer.append(count)
+#                 tree[name] = count
+    
+#     return answer
--- 복습/test.py
+++ 복습/test.py
@@ -1,53 +1,15 @@
-'''
-집에 공유기설치한다. 공유기가 C개 설치.
-가장 가까운 공유기 사이의 거리가 최대로 해야함
+from bisect import bisect_left, bisect_right
 
-1. 집마다 거리 1~제일긴거리 이 사이로 공유기 설치한다.
-2. 공유기를 C개 이상 설치할 수 있다면 거리 늘려봄
-3. 공유기를 C개 보다 작게 설치할 수 있다면 거리 줄여봄
-'''
-import sys
-input = sys.stdin.readline
+def search(data, m):
+    left_index = bisect_left(data, m)
+    right_index = bisect_right(data, m)
+    print(left_index, right_index)
+    return right_index - left_index
 
-n, c = map(int, input().split())
-array = [int(input()) for _ in range(n)]
-array.sort() # 20만 x 약15 = 300만
-
-# 예외처리
-if c >= n: # 공유기 대수가 더 많으면 가장 인접한애 거리 출력한다.
-    min_value = int(1e9)
-    prev = array[0]
-    for i in range(1, len(array)):
-        min_value = min(min_value, array[i] - prev)
-        prev = array[i]
-    print(min_value)
-    exit(0)
-
-# 거리로 접근 할 것이다.
-start = 1 # 제일짧은거리
-end = array[-1] - array[0]
-
-result = 0
-while start <= end:
-    mid = (start + end) // 2 # mid마다 공유기 설치
-
-    # 1번. 첫 집부터 끝집까지 mid이상위치에 공유기 설치
-    prev = array[0]
-    count = 1 # 설치한 공유기 개수를 셀 변수 (0번째집에는 설치)
-    for i in range(1,len(array)):
-        if array[i] - prev >= mid: # 공유기 설치할 수 있는 집 발견
-            prev = array[i] # 현재 위치 바꿈
-            count += 1
-            if count == c: # 더 안봐도됨
-                break
-
-    # 2번. mid길이로 다 설치했을 때 설치한 공유기 대수를 확인
-    if count == c: # 더 설치할 수 있다면 길이 늘려봄
-        result = mid
-        start = mid + 1
-    else:
-        end = mid - 1
-
-print(result)
-
-
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+result = search(data,m)
+if result == 0: # 값이 없음
+    print(-1)
+else:
+    print(result)
