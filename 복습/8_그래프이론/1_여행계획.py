60927b392b990538cfddeccfea815b179c6dfb78
Minsik113 <hyunicho62@gmail.com>
2021-03-17 20:21:07 +0900

    3/17 그래프이론 복습

--- 복습/8_그래프이론/1_여행계획.py
+++ 복습/8_그래프이론/1_여행계획.py
@@ -1,53 +1,58 @@
+##########################################
+##########################################
+# 3/17 복습
 '''
-전체 도시에 대해 union find해서 
-여행 도시들이 모두 같은 집합인지 체크하면된다.
-
+array에따라 연결된 애들을 union_find하고 data있는애들이 같은 parent인지보자?
 '''
-########################################
-########################################
-# 좀더 깔끔한 코드
-def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
-
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
-    if a < b:
-        parent[b] = a
-    else:
+    if a > b:
         parent[a] = b
+    else:
+        parent[b] = a
+
+def find_parent(parent, x):
+    if parent[x] != x: # 대표값이 아니라면 대표값으로 바꿔줌
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
 n, m = map(int, input().split())
-parent = [0] * (n+1)
+array = [list(map(int, input().split())) for _ in range(n)]
+data = list(map(int, input().split()))
 
-# 부모 초기화
+# 대표값 선언 및 초가화
+parent = [0] * (n+1)
 for i in range(1, n+1):
     parent[i] = i
 
-# 도로상황 입력받기
+# 연결되어있나 확인하자
 for i in range(n):
-    chart = list(map(int, input().split()))
-    for j in range(n):
-        if chart[j] == 1:
-            union_parent(parent, i, j)
+    for j in range(i, n):
+        if array[i][j] == 1: # 연결이 되어있다면 i,j 연결
+            union_parent(parent, i+1, j+1)
 
-array = list(map(int, input().split()))
-
-result = True
-for i in range(m-1):
-    if find_parent(parent, array[i]) != find_parent(parent, array[i+1]):
-        result = False
+pre = data[0]
+flag = True
+for i in range(1, len(data)):
+    if parent[pre] == parent[data[i]]:
+        pre = data[i]
+    else:
+        flag = False
         break
-
-if result:
-    print("YES")
-else:
+print(parent)    
+if not flag: # 안이어졌다면
     print("NO")
-########################################
-########################################
-# 내풀이
+else:
+    print("YES")
+
+# '''
+# 전체 도시에 대해 union find해서 
+# 여행 도시들이 모두 같은 집합인지 체크하면된다.
+# '''
+# ########################################
+# ########################################
+# # 좀더 깔끔한 코드
 # def find_parent(parent, x):
 #     if parent[x] != x:
 #         parent[x] = find_parent(parent, parent[x])
@@ -63,7 +68,6 @@ else:
 
 # n, m = map(int, input().split())
 # parent = [0] * (n+1)
-# chart = []
 
 # # 부모 초기화
 # for i in range(1, n+1):
@@ -71,24 +75,67 @@ else:
 
 # # 도로상황 입력받기
 # for i in range(n):
-#     chart.append(list(map(int, input().split())))
-
-# # chart[a][b] == 1이면 a와 b의 부모 같게한다.
-# for i in range(n):
-#     for j in range(i,n):
-#         if chart[i][j] == 1:
+#     chart = list(map(int, input().split()))
+#     for j in range(n):
+#         if chart[j] == 1:
 #             union_parent(parent, i, j)
 
 # array = list(map(int, input().split()))
 
-# check = True
-# start = find_parent(parent, array[0])
-# for i in range(1, len(array)):
-#     go = find_parent(parent, array[i])
-#     if start != go:
-#         check = False
+# result = True
+# for i in range(m-1):
+#     if find_parent(parent, array[i]) != find_parent(parent, array[i+1]):
+#         result = False
 #         break
-# if check:
+
+# if result:
 #     print("YES")
 # else:
 #     print("NO")
+# ########################################
+# ########################################
+# # 내풀이
+# # def find_parent(parent, x):
+# #     if parent[x] != x:
+# #         parent[x] = find_parent(parent, parent[x])
+# #     return parent[x]
+
+# # def union_parent(parent, a, b):
+# #     a = find_parent(parent, a)
+# #     b = find_parent(parent, b)
+# #     if a < b:
+# #         parent[b] = a
+# #     else:
+# #         parent[a] = b
+
+# # n, m = map(int, input().split())
+# # parent = [0] * (n+1)
+# # chart = []
+
+# # # 부모 초기화
+# # for i in range(1, n+1):
+# #     parent[i] = i
+
+# # # 도로상황 입력받기
+# # for i in range(n):
+# #     chart.append(list(map(int, input().split())))
+
+# # # chart[a][b] == 1이면 a와 b의 부모 같게한다.
+# # for i in range(n):
+# #     for j in range(i,n):
+# #         if chart[i][j] == 1:
+# #             union_parent(parent, i, j)
+
+# # array = list(map(int, input().split()))
+
+# # check = True
+# # start = find_parent(parent, array[0])
+# # for i in range(1, len(array)):
+# #     go = find_parent(parent, array[i])
+# #     if start != go:
+# #         check = False
+# #         break
+# # if check:
+# #     print("YES")
+# # else:
+# #     print("NO")
--- 복습/test.py
+++ 복습/test.py
@@ -1,5 +1,38 @@
-a = [1,2,3,4,5]
+'''
+탑승구에 비행기 넣기
+'''
 
-s = 'werawer'
-s = s.replace('w','z')
-print(s)
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+g = int(input()) # 탑승구 수
+p = int(input()) # 비행기 수
+
+planes = [int(input()) for _ in range(p)]
+
+# 대표값 초기화
+# visited = [False] * (g+1)
+parent = [0] * (g+1) # i번째 공항아래로 댈 수 있는 비행기 수
+for i in range(1, g+1):
+    parent[i] = i 
+
+# 비행기 넣어보자
+count = 0
+for plane in planes:
+    # 공항의 max값보다 번호 큰 비행기가 들어오면 맨 끝쪽에
+    check = find_parent(parent, plane)
+    if check == 0: # 현재 루트가 0이라면
+        break
+    union_parent(parent, check, check-1)
+    count += 1
+print(count)
