4ff692e91d469d7a46c74fd83c7cd344f1b1ef8e
Minsik113 <hyunicho62@gmail.com>
2021-03-21 18:01:00 +0900

    3/21 백준 백트래킹

--- 복습/test.py
+++ 복습/test.py
@@ -1,32 +1,48 @@
-from itertools import combinations
+'''
+같거나 작으면안된다
+큰애들이랑만 짝가능하다.
+
+두 팀으로 나눠서 팀끼리의 합을 구해라.
+총합의 차중 가장 작은값은?
+
+'''
 
 n = int(input())
-data = [list(map(int, input().split())) for _ in range(n)]
-array = [i for i in range(n)]
-min_value = int(1e9)
-total = set()
+array = [list(map(int, input().split())) for _ in range(n)]
 
-for left in combinations(array, n//2):
-    right = set()
-    right = []
+row = []
+
+# x를 넣어도되는지 본다. 오름차순이어야함
+def check(x):
+    # 먼저 들어가있는것들이 크거나 같으면 안됨.
+    for j in range(len(row)): 
+        if row[j] >= x:
+            return False
+    return True
+
+# 팀을 구하자
+min_value = int(1e9)
+def solve():
+    global min_value
+    # 팀완성되었으면 합을 구하자
+    if len(row) == n//2:
+        one = 0
+        theother = 0
+        for a in range(n):
+            for b in range(a+1, n):
+                if a == b:
+                    continue
+                if a in row and b in row:
+                    one += array[a][b] + array[b][a]
+                elif a not in row and b not in row:
+                    theother += array[a][b] + array[b][a]
+        min_value = min(min_value, abs(one-theother))
+    # 팀 완성안되었으면 넣으면됨
     for i in range(n):
-        if i not in left:
-            right.append(i)
-    # 봤던거 안보기 위해서
-    if left in total:
-        break
-    total.add(left)
-    total.add(tuple(right))
-    
-    left = list(left)
-    right = list(right)
-    # left, right 합구하자
-    l_sum, r_sum = 0, 0
-    for i in range(len(left)):
-        for j in range(len(right)):
-            l_sum += data[left[i]][left[j]]
-            r_sum += data[right[i]][right[j]]
-    print(left, right, l_sum, r_sum)
-    min_value = min(min_value, abs(l_sum - r_sum))
-print()
+        if check(i): # i를 넣어도 될까?
+            row.append(i)
+            solve() # 다음위치에 넣을꺼 찾아보자
+            row.pop() #다시초기화
+
+solve()
 print(min_value)
--- 복습/백트래킹/19_14889_스타트와링크.py
+++ 복습/백트래킹/19_14889_스타트와링크.py
@@ -1,6 +1,56 @@
 ##########################################
 ##########################################
-# combination풀이
+# 3/21 pypy3로는 됨
+'''
+같거나 작으면안된다
+큰애들이랑만 짝가능하다.
+두 팀으로 나눠서 팀끼리의 합을 구해라.
+총합의 차중 가장 작은값은?
+'''
+
+n = int(input())
+array = [list(map(int, input().split())) for _ in range(n)]
+
+row = []
+
+# x를 넣어도되는지 본다. 오름차순이어야함
+def check(x):
+    # 먼저 들어가있는것들이 크거나 같으면 안됨.
+    for j in range(len(row)): 
+        if row[j] >= x:
+            return False
+    return True
+
+# 팀을 구하자
+min_value = int(1e9)
+def solve():
+    global min_value
+    # 팀완성되었으면 합을 구하자
+    if len(row) == n//2:
+        one = 0
+        theother = 0
+        for a in range(n):
+            for b in range(a+1, n):
+                if a == b:
+                    continue
+                if a in row and b in row:
+                    one += array[a][b] + array[b][a]
+                elif a not in row and b not in row:
+                    theother += array[a][b] + array[b][a]
+        min_value = min(min_value, abs(one-theother))
+    # 팀 완성안되었으면 넣으면됨
+    for i in range(n):
+        if check(i): # i를 넣어도 될까?
+            row.append(i)
+            solve() # 다음위치에 넣을꺼 찾아보자
+            row.pop() #다시초기화
+
+solve()
+print(min_value)
+
+##########################################
+##########################################
+# 3/21 combination풀이
 from itertools import combinations
 
 n = int(input())
@@ -34,68 +84,36 @@ for left in combinations(array, n//2):
 print(min_value)
 ##########################################
 ##########################################
-# 백트래킹 은 왜케안풀림? 시간초과
-# '''
-# n = 짝수
-# n명을 반띵해라.
-# 1. a팀 n//2명 을뽑아라.
-# 2. b팀은 나머지
-# 3. a팀에 속한애들 a_sum 에두고 
-# 4. a팀에 안속하면 b_sum에 더한다.
-# 5. abs(a_sum - b_sum)이 제일 작은값을 구해라
-# '''
-# import sys
-# input = sys.stdin.readline
+# 3/20 pypy3로는 됨
+'''
+n = 짝수
+n명을 반띵해라.
+1. a팀 n//2명 을뽑아라.
+2. b팀은 나머지
+3. a팀에 속한애들 a_sum 에두고 
+4. a팀에 안속하면 b_sum에 더한다.
+5. abs(a_sum - b_sum)이 제일 작은값을 구해라
+'''
+from itertools import combinations
 
-# min_value = int(1e9)
-# n = int(input())
-# data = [list(map(int, input().split())) for _ in range(n)]  # 번호 0~n-1
-# left = []
-# count = [1] * n
-# # x가 left에 들어가도 되는가? 
-# def check(x, depth):
-    
-#     # 넣을 수 있다.
-#     if count[x] == 1: 
-#         for i in range(depth): # depth아래로 x보다 큰 수가 나오면 불가
-#             if left[i] > x:
-#                 return False
-#         # depth아래로 x보다 큰수가 없으므로 left에 추가
-#         return True
-#     # 넣을 수 없으면 False
-#     return False
+n = int(input())
+data = [list(map(int, input().split())) for _ in range(n)]
+array = [i for i in range(n)]
+min_value = int(1e9)
 
-# def solve(depth):
-#     global min_value
-#     # left다 찼다. 합들구하면됨
-#     if depth == n//2:
-#         l_sum = 0
-#         r_sum = 0
-#         right = []
-#         # right 구한다
-#         for i in range(n):
-#             if i not in left:
-#                 right.append(i)
-#         # 합들을 구해주자
-#         for i in range(len(left)):
-#             for j in range(n//2):
-#                 if i == j:
-#                     continue
-#                 l_sum += data[left[i]][left[j]]
-#                 r_sum += data[right[i]][right[j]]
-#         # min값
-#         min_value = min(min_value, abs(l_sum-r_sum))
-#         print(left,right,l_sum,r_sum)
-#         return
-#     # 0~n번까지 수를 넣으면됨
-#     for i in range(n):
-        
-#         if check(i, depth): # i넣어도됨?
-#             count[i] -= 1
-#             left.append(i)
-#             solve(depth + 1)
-#             count[i] += 1
-#             left.pop()
-        
-# solve(0)
-# print(min_value)
+for left in combinations(array, n//2):
+    left = list(left)
+    right = []
+    for i in range(n):
+        if i not in left:
+            right.append(i)
+    # left, right 합구하자
+    l_sum, r_sum = 0, 0
+    for i in range(len(left)):
+        for j in range(len(right)):
+            l_sum += data[left[i]][left[j]]
+            r_sum += data[right[i]][right[j]]
+    # print(left, right, l_sum, r_sum)
+    min_value = min(min_value, abs(l_sum - r_sum))
+
+print(min_value)
