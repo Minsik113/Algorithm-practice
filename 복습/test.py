d34c2dbc371fa531f682e3bb5bea8cdb9a60db5c
Minsik113 <hyunicho62@gmail.com>
2021-03-24 17:53:35 +0900

    3/24 백준 스택 복습

--- 복습/test.py
+++ 복습/test.py
@@ -1,79 +1,48 @@
 '''
-맵: nxn 
-m마리의 물고기
-(시작)아기상어크기 2
-상하좌우로 이동
-
-현재위치에서 도달가능한 물고기들 중 먹을 수 있는애들을 봐야한다 
-1. 먹을 수 있는 물고기가 없다면 -> 엄마상어
-2. 먹을 수 있는 물고기 1개 -> 먹으러간다
-3. 먹을 수 있는 물고기가 2개이상 -> 거리가 가까운 물고기 먹으러감
-(위쪽, 왼쪽 에 우선순위를 둔다)
-
-먹으면 빈칸이됨.
-먹은수(count)가 자신의 크기와 같아지면 크기 +1
-엄마상어부를때까지 걸리는 초는?
+그냥 짝이 아니면 no라고하면된다. 
+짜증나네
 '''
-from collections import deque
-import heapq
 
-n = int(input())
-
-# 맵 입력받자
-array = [list(map(int, input().split())) for _ in range(n)]
-
-# 아기상어 위치, 물고기 들어있는 곳
-start_pos = [0,0]
-fish = [] # 물고기들 무게 1~6,좌표
-for i in range(n):
-    for j in range(n):
-        if array[i][j] == 9:
-            start_pos[0], start_pos[1] = i,j
-            # array[i][j] = -2 # 상어표시
-        elif array[i][j] != 0:
-            heapq.heappush(fish, (array[i][j], i, j))
-
-print(fish)
-exit()
-# 현재위치에서 도달가능한 물고기들 중 먹을 수 있는애들을 봐야한다
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-# fish = (물고기크기, 좌표)
-# 현재위치에서 먹을 수 있는애 찾아보자
-result = 0 # 전체 걸린시간
-
-q = deque()
-q.append((0, start_pos[0], start_pos[1])) # 좌표, 시간
-now = 2
-possible = deque() # 현재 먹을 수 있는 애들
+s = []
+flag = True
 while True:
-    while q: # (여기까지걸리는시간, 좌표)
-        time, x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:
-                continue
-            # 이동가능한 애들중에서 먹을 수 있는애 찾자
-            if array[nx][ny] == 0:
-                q.append((time+1, nx, ny))
-            elif array[nx][ny] <= now: # 현재무게에서 먹을수있는애들
-                possible.append((time+1, nx, ny)) # 
-                q.append((time+1, nx, ny))
-    # 먹을 수 있는애들의 개수가 1 or 2이상
-    if len(possible) == 0: # 종료
-        print(result)
-        break
-
-    possible.sort() # 오름차순 순서로 됨
-    elif len(possible) >= 1:
-        while possible: # 가능한애들 다 먹는다
-            zzz = possible.popleft() # 제일 조금걸리는애한테 감.
-            result += zzz[0] # 전체시간 증가시키고,
-            # 갈수있는애들중 가까운애한테 다시가야함
-            for
-
-        # 좌표이동 시키고
-        # 물고기 위치 0으로바꾸고
-        # count + 1 . 몸집커지나도봐야함
-
+    string = input()
+    for i in range(len(string)):
+        # 열음
+        if string[i] == '(' or string[i] == '[':
+            s.append(string[i])
+        # 닫음
+        elif string[i] == ')':
+            # 1. 짝이 아니니까 no이다. '.'나올때까지 보자
+            if len(s) == 0: 
+                # print("1") 
+                flag = False
+            else:
+                k = s.pop()
+                if k != '(':
+                    # print("2")
+                    flag = False
+        # 닫음
+        elif string[i] == ']':
+            # 2. 짝이 아니니까 no이다. '.'나올때까지 보자
+            if len(s) == 0: 
+                # print("3") 
+                flag = False
+            else:
+                k = s.pop()
+                if k != '[':
+                    # print("4")
+                    flag = False
+        elif string[i] == '.':
+            # 3번. 짝이 안맞음 => no
+            if not flag: 
+                print("no") 
+            # 4번. 짝맞음 => 길이가 남아있으면 no임
+            elif len(s) != 0: 
+                print("no") 
+            # 5번. 짝맞음 => 길이가 0이면 yes지
+            else:
+                print("yes")
+            # print(s,flag)
+            s = [] # 스택초기화                
+            flag = True # flag초기화
--- 복습/test2.py
+++ 복습/test2.py
@@ -1,70 +1,29 @@
-##########################################
-##########################################
-# 3/
-
-##########################################
-##########################################
-#
-'''
-mst문제네.
-=> 대각선의 개수 = n(n-3)/2 이므로 모든 거리를 구하면 10만^2가됨. 불가능
-필요한것만 저장해야한다.
-
-두행성의 비용구한다
-edges = [비용,x,y] 식으로 저장
--> heapq이용해서 하나씩 pop하면서 보자
-
-'''
-import sys, heapq
-input = sys.stdin.readline
-
-def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
-
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a < b:
-        parent[b] = a
+while True:
+    s = []
+    flag = True
+    string = input()
+    # 종료조건
+    if string == '.': 
+        break
+    # 
+    for i in string:
+        if i == '(' or i == '[':
+            s.append(i)
+        elif i == ')':
+            if not s or s[-1] == '[':
+                flag = False
+                break
+            elif s[-1] == '(':
+                s.pop()
+        elif i == ']':
+            if not s or s[-1] == '(':
+                flag = False
+                break
+            elif s[-1] == '[':
+                s.pop()
+    if flag and (not s): # flag == True and s가비어있으면yes
+        print("yes")
     else:
-        parent[a] = b
+        print("no")
 
-n = int(input())
-
-# 부모 선언 및 초기화
-parent = [0] * (n+1) # 1~n
-for i in range(1, n+1):
-    parent[i] = i
-
-# 1번. 행성들의 좌표
-x = []
-y = []
-z = []
-for i in range(1, n+1):
-    a, b, c = map(int, input().split())
-    x.append((a,i))
-    y.append((b,i))
-    z.append((c,i))
-x.sort()
-y.sort()
-z.sort()
-
-# 2번. 별->별 까지의 거리.
-edges = []
-for i in range(n-1):
-    edges.append((x[i+1][0] - x[i][0], x[i][1], x[i+1][1]))
-    edges.append((y[i+1][0] - y[i][0], y[i][1], y[i+1][1]))
-    edges.append((z[i+1][0] - z[i][0], z[i][1], z[i+1][1]))
-
-
-
-# 3번. MST START 
-result = 0
-while h:
-    cost, x, y = heapq.heappop(h)
-    if find_parent(parent, x) != find_parent(parent, y):
-        union_parent(parent, x, y)
-        result += cost
-print(result)
+        
