d9745b0cac71009509df9cace620129008a90fc5
minsik113 <hyunicho62@gmail.com>
2022-11-30 20:59:17 +0900

    누락소스

--- 프로그래머스_코딩테스트입문_하루4문제/Day4_수학_배열/피자 나눠 먹기1.py
+++ 프로그래머스_코딩테스트입문_하루4문제/Day4_수학_배열/피자 나눠 먹기1.py
@@ -4,7 +4,7 @@
 
 문제:
 풀이:
-22:06~
+22:06~22:08
 n을 7로 나눈 몫 a, 나머지가 0이면 -> a개의 피자필요
 n을 7로 나눈 몫 a, 나머지가 b이면 -> a+1개의 피자필요
 '''
@@ -13,6 +13,6 @@ def solution(n):
     if n % 7 == 0:
         return a
     return a+1
-    
+
 def solution(n):
     return n//7 + (1 if n%7 else 0)
