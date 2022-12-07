3d97cef9aa8832a54fcac2d96db8b6267680e087
minsik113 <hyunicho62@gmail.com>
2022-12-07 23:11:29 +0900

    Day11

---
+++ 프로그래머스_코딩테스트입문_하루4문제/Day11_수학/팩토리얼.py
@@ -0,0 +1,25 @@
+'''
+날짜: 2022.12.07
+시간복잡도:
+
+문제:
+풀이:
+한 번 봤던거를 또 보게하면 안된다. -> 메모라이즈 사용
+'''
+def solution(n):
+    answer = 0
+    m = [0] * 11 # 1~10팩토리얼까지 저장
+    m[1] = 1
+    m[2] = 2
+    
+    for i in range(1,11):
+        if m[i] == 0:
+            m[i] = m[i-1] *i
+            
+        if n <= m[i]:
+            if n == m[i]:
+                answer=i
+            else:
+                answer=i-1
+            break
+    return answer
