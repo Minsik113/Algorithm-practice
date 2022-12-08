4f0c22038b6d4c9e24ec3f6600ffd596447c735b
minsik113 <hyunicho62@gmail.com>
2022-12-08 23:59:20 +0900

    day11

---
+++ 프로그래머스_코딩테스트입문_하루4문제/Day11_수학/최댓값 만들기1.py
@@ -0,0 +1,22 @@
+'''
+날짜: 2022.12.08
+시간복잡도:
+
+문제:
+풀이:
+제일큰 수 2개 구하면됨.
+최대힙으로 빼면 넣는거만 생각하면됨 O(logN+2) # N개 넣고 2개 뽑고
+
+'''
+import heapq
+def solution(numbers):
+    answer = 0
+    h = []
+    for i in numbers:
+        heapq.heappush(h,-i)
+    a=heapq.heappop(h)
+    b=heapq.heappop(h)
+    print(h)
+    print(-a,-b)
+    
+    return -a*-b
