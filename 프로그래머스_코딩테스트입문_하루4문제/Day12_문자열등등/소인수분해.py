129453f8b63cdd50b7bedd07a022a1e543009d61
minsik113 <hyunicho62@gmail.com>
2022-12-13 21:27:27 +0900

    소인수분해 문제 해결. 시간줄이려고 노력함

--- 프로그래머스_코딩테스트입문_하루4문제/Day12_문자열등등/소인수분해.py
+++ 프로그래머스_코딩테스트입문_하루4문제/Day12_문자열등등/소인수분해.py
@@ -5,44 +5,51 @@
 문제:
 풀이:
 소인수분해해라 -> 약수 set을 찾아라
-나온 숫자를 소인수분해 한 dict이 필요하다. 이걸보면서 최소한으로 체크해야함.
+모든약수에서 소수인것만 찾으면된다?
+
 '''
 import math
+def prime_number(num): # True -> 소수, False -> 소수x
+    flag = True
+    for i in range(2, int(math.sqrt(num))+1):
+        if num % i == 0:
+            flag = False
+            break
+    if flag: return True
+    else: return False
+    
+def solution(n):
+    answer = set()
+    end = int(math.sqrt(n))+1
+    
+    for i in range(2, end):
+        if n % i == 0:
+            print(i,n//i)
+            # i가 소수인지 검사
+            if (i not in answer) and prime_number(i):
+                answer.add(i)
+            # n//i가 소수인지 검사
+            if (n//i not in answer) and prime_number(n//i):
+                answer.add(n//i)
+    
+    if n not in answer and prime_number(n):
+        answer.add(n)
+        
+    # 겹치는게 있을 수도 있다.
+    return list(sorted(answer))
+
+###
+# 아래처럼 풀면 훨씬 오래걸린다.
+###
 def solution(n):
     answer = []
-    real = dict() # 해당 숫
-    for i in range(1, int(math.sqrt(n))+1):
-        if n % i == 0:
-            print(i, n//i)
-        
+    d = 2
+    while d <= n:
+        if n % d == 0:
+            n /= d
+            if d not in answer:
+                answer.append(d)
+        else:
+            d += 1
+                
     return answer
-    
-# def check(n):
-#     cnt = 0 
-#     res = []
-#     for i in range(2,n):
-#         if n % i == 0: # i로 나누어짐
-#             res = [i, n//i]
-#             cnt = 1
-#             print(cnt,res)
-#             break
-#     if cnt == 1:
-#         return res
-#     else:
-#         return [0,0]
-    
-# def solution(n):
-#     answer = []
-#     answer_set = set()
-#     z = check(n)
-#     # 1외에 약수 없으면 출력
-#     if z[0] == 0:
-#         return [n]
-#     # 반복문으로 넣어야한다.
-#     while True:
-#         if z[0] == 0:
-#             break
-#         answer_set.add(z[0])
-#         z = check(z[1])
-#     print(answer_set)
-#     return answer
