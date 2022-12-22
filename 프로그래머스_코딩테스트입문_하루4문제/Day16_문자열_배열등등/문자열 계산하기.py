41831d979c6da0e849e5260377ddaf42dd1f9652
minsik113 <hyunicho62@gmail.com>
2022-12-22 23:43:51 +0900

    Day16쉬움

---
+++ 프로그래머스_코딩테스트입문_하루4문제/Day16_문자열_배열등등/문자열 계산하기.py
@@ -0,0 +1,11 @@
+'''
+날짜: 2022.12.22
+시간복잡도:
+
+문제:
+풀이:
+23:12~23:14
+eval()쓰면됨
+'''
+def solution(my_string):
+    return eval(my_string)
---
+++ 프로그래머스_코딩테스트입문_하루4문제/Day16_문자열_배열등등/편지.py
@@ -0,0 +1,10 @@
+'''
+날짜: 2022.12.22
+시간복잡도:
+
+문제:
+풀이:
+23:42~23:43(1분)
+'''
+def solution(message):
+    return len(message)*2
