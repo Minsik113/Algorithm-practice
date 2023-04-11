91be00cd3a9c6a29f93616e2142539dbbb6e8f2d
minsik113 <hyunicho62@gmail.com>
2023-04-11 23:53:02 +0900

    001-숫자합구하기

--- [Book1]Doit_Algorithm_Codingtest/01-구간합/001-숫자의 합 구하기.py
+++ [Book1]Doit_Algorithm_Codingtest/01-구간합/001-숫자의 합 구하기.py
@@ -1,9 +1,25 @@
 '''
-날짜: 2022.09.29
-시간복잡도:
+날짜: 2023.04.11
+시간복잡도: O(N)
 
 문제: https://www.acmicpc.net/problem/11720
-풀이방식:
+풀이방식:브루트포스
+'''
+n = int(input())
+arr = input()
+print(type(arr))
+answer = 0
+for i in range(n):
+    answer += int(arr[i])
+print(answer)
+
+
+'''
+날짜: 2022.09.29
+시간복잡도: O(N)
+
+문제: https://www.acmicpc.net/problem/11720
+풀이방식: 브루트포스
 '''
 n = int(input())
 number = input()
