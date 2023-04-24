244de256bf63a9163bb49473af49c9355bf0ff4e
minsik113 <hyunicho62@gmail.com>
2023-04-25 00:33:50 +0900

    2023 04 25 부분합 생각보다 어렵네...

--- [Book1]Doit_Algorithm_Codingtest/01-구간합/nA003-구간 합 구하기1.py
+++ [Book1]Doit_Algorithm_Codingtest/01-구간합/nA003-구간 합 구하기1.py
@@ -6,11 +6,25 @@
 idx가 a~b인 애들의 합을 구해라
 
 풀이방식:
+23:47~
+'''
+
+
+
+
+
+'''
+날짜: 2022.09.29
+시간복잡도: O(N)
+
+문제: https://www.acmicpc.net/problem/11659
+idx가 a~b인 애들의 합을 구해라
+
+풀이방식:
 19:30 ~ 19:50
 
 부분합을 저장해두는 dictionary를 만들자.
 값이 있으면 쓰고, 없으면 생성
-
 '''
 import sys
 input = sys.stdin.readline
@@ -30,3 +44,24 @@ for i in range(m):
         print(list_sum_arr[b-1] - 0)
     else:
         print(list_sum_arr[b-1] - list_sum_arr[a-2])
+
+
+'''
+아래 방법이 제일 괜찮아보인다.
+'''
+# import sys
+# input = sys.stdin.readline
+
+# n, m = map(int, input().split())
+# arr = list(map(int, input().split()))
+# s = [0]
+# temp = 0 
+
+# # 부분합
+# for i in arr:
+#     temp = temp+i
+#     s.append(temp)
+
+# for _ in range(m):
+#     a, b = map(int, input().split())
+#     print(s[b]-s[a-1])
--- [Book1]Doit_Algorithm_Codingtest/test1.py
+++ [Book1]Doit_Algorithm_Codingtest/test1.py
@@ -1,3 +1,20 @@
-def exam(n,a=2):
-    print('a=',n,'b=',a)
-exam(20)
+'''
+왜 도대체 뭐가잘못된걸까
+
+'''
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+s = [0]
+temp = 0 
+
+# 부분합
+for i in arr:
+    temp = temp+i
+    s.append(temp)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    print(s[b]-s[a-1])
