118fe956ea2f97be9b2b67eaf344b0d4bf6aecf0
minsik113 <hyunicho62@gmail.com>
2023-10-25 21:35:04 +0900

    int(숫자,진수) , *divmod(a,b):몫,나머지 반환

--- 프로그래머스_파이썬을 파이썬답게/Part2-2-n진법표현.py
+++ 프로그래머스_파이썬을 파이썬답게/Part2-2-n진법표현.py
@@ -9,24 +9,24 @@
 ####################
 
 #1. 처음풀이. -> 파이썬스럽지 않음. 
-num, base = map(int, input().strip().split(' '))
+# num, base = map(int, input().strip().split(' '))
 
-idx = 0
-answer = 0
-strNum = str(num)
-for i in range(len(strNum), 0, -1):
-    answer += int(strNum[i-1]) * (base**idx)
-    idx+=1
-print(answer)
+# idx = 0
+# answer = 0
+# strNum = str(num)
+# for i in range(len(strNum), 0, -1):
+#     answer += int(strNum[i-1]) * (base**idx)
+#     idx+=1
+# print(answer)
 
 #2. 1번보다 간단히. 하지만 역시 파이썬스럽진않음 -> enumerate함수를 이용해서 위보다 간단하게 풀기
-num, base = map(int, input().strip().split(' '))
+# num, base = map(int, input().strip().split(' '))
 
-answer = 0
-num = str(num)
-for idx, number in enumerate(num[::-1]):
-    answer += int(number) * (base **idx)
-print(answer)
+# answer = 0
+# num = str(num)
+# for idx, number in enumerate(num[::-1]):
+#     answer += int(number) * (base **idx)
+# print(answer)
 
 #3. 파이썬 내장함수 사용. ->  int(x, base=진수)
 num, base = map(int, input().strip().split(' '))
